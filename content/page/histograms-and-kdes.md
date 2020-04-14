2524726457c6c758eb11c045b6ac43e8b1feb7fb
Julian Wergieluk <julian@wergieluk.com>
2020-04-14 14:14:53 +0200

    Add rectangle construction plots

--- content/page/histograms-and-kdes.md
+++ content/page/histograms-and-kdes.md
@@ -35,10 +35,23 @@ For that I can just sort the data points and plot the values.
 
 {{< figure src="/histograms-and-kdes/x-axis.png" >}}
 
-The problem with this visualization is that many values, like 30, are repeated and
-plotted on top of each other: There is no way to tell how many 30 minutes sessions
-do I have in the data set. 
+The problem with this visualization is that many values are repeated and
+plotted on top of each other: There is no way to tell how many 30 minute sessions
+do I have in the data set. An idea, that leads to the construction of 
+a histogram, is to use the vertical dimension of the plot distiguish between
+regions with different data density. 
 
+Let's divide the data range into intervals: 
 
+    [10, 20), [20, 30), [30, 40), [40, 50), [50, 60), [60, 70), 
 
+For each data point in the first interval `[10, 20)` I put a rectangle
+with area `1/129` (approx. `0.007`) and width 10 on the interval `[10, 20)`. 
+It's like stacking Jenga bricks. Since we have 13 data point in the 
+interval `[10, 20)` the stacked rectangles have the height of approx.
+`0.01`:
+
+{{< figure src="/histograms-and-kdes/histogram-construction-1.png" >}}
+
+{{< figure src="/histograms-and-kdes/histogram-construction-2.png" >}}
 
--- static/histograms-and-kdes/meditation.py
+++ static/histograms-and-kdes/meditation.py
@@ -2,6 +2,7 @@ import numpy
 import pandas
 import matplotlib.pyplot as plt
 import matplotlib
+import matplotlib.patches
 import os
 import pathlib
 
@@ -18,11 +19,38 @@ def main():
 
     plt.figure(figsize=(6, 1.5))
     plt.scatter(x=df.iloc[:, 0], y=numpy.repeat(0.0, len(df)) , marker='*',
-                     alpha=0.35, s=matplotlib.rcParams['lines.markersize']*20)
+                alpha=0.35, s=matplotlib.rcParams['lines.markersize']*20)
     ax = plt.gca()
+    ax.add_patch(matplotlib.patches.Rectangle((10, 0), 10.0, 1.0))
     ax.get_yaxis().set_ticks([])
     plt.tight_layout()
     plt.savefig(to_abs('x-axis.png'), dpi=150)
+    plt.close('all')
+
+    plt.figure(figsize=(6, 1.5))
+    plt.scatter(x=df.iloc[:, 0], y=numpy.repeat(0.0, len(df)) , marker='*',
+                alpha=0.35, s=matplotlib.rcParams['lines.markersize']*20)
+    plt.ylim((-0.01, 0.02))
+    ax = plt.gca()
+    range_count = len(list(i for i in df.iloc[:, 0] if 10.0 <= i < 20.0))
+    ax.add_patch(matplotlib.patches.Rectangle((10.0, 0.0), 10.0, range_count/1290.0, alpha=0.4))
+    # ax.get_yaxis().set_ticks([])
+    plt.tight_layout()
+    plt.savefig(to_abs('histogram-construction-1.png'), dpi=150)
+    plt.close('all')
+
+    histogram_data = {i: len(list(x for x in df.iloc[:, 0] if i <= x < i + 10.0))
+                      for i in range(10, 80, 10)}
+
+    plt.figure(figsize=(6, 2.5))
+    plt.scatter(x=df.iloc[:, 0], y=numpy.repeat(0.0, len(df)), marker='*',
+                alpha=0.35, s=matplotlib.rcParams['lines.markersize'] * 20)
+    plt.ylim((-0.01, 0.06))
+    ax = plt.gca()
+    for x0, h in histogram_data.items():
+        ax.add_patch(matplotlib.patches.Rectangle((x0, 0.0), 10.0, h / 1290.0, alpha=0.4))
+    plt.tight_layout()
+    plt.savefig(to_abs('histogram-construction-2.png'), dpi=150)
     plt.close('all')
 
 
